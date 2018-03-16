21b454a19d4d4c8a49404842e88af8b00bc95788
AdityaCheruvu <adityacheruvucs@gmail.com>
2018-03-16 12:29:17 +0530

    CPU Done

--- CpuUsage.cpp
+++ CpuUsage.cpp
@@ -3,6 +3,8 @@
 #include<sstream>
 #include<string>
 #include<unistd.h>
+#include<signal.h>
+#include<stdlib.h>
 using namespace std;
 int start[5][2], end[5][2];
 double perc[5];
@@ -25,8 +27,8 @@ void CalcForAllCpu()
 			tot_jiffies+=k;
 		}
 		start[cpu][0] = work_jiffies;
-		start[cpu][1] = tot_jiffies;	
-		
+		start[cpu][1] = tot_jiffies;
+
 	}
 	cpuStat.close();
 	usleep(1000000);
@@ -50,8 +52,15 @@ void CalcForAllCpu()
 		perc[i] = double((end[i][0] - start[i][0]))/double((end[i][1] - start[i][1]));
 }
 
+void handle_sig(int sig)
+{
+	cout << "\033[2J\033[1;1H";
+	exit(1);
+}
+
 int main()
 {
+	signal(SIGINT, handle_sig);
 	while(1)
 	{
 		CalcForAllCpu();
@@ -60,6 +69,6 @@ int main()
 		for(int i=1;i<5;i++)
 			cout<<"CPU "<<i-1<<": "<<perc[i]*100<<"%"<<endl;
 	}
-	
+
 
 }
